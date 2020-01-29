e15a1893df2768de38164dc2405db8f6ab16b4e8
Danilo Pereira <danilopx1@gmail.com>
2020-01-29 00:23:22 -0300

    Installing and configuring Docker, Sequelize, ESLint, Prettier and EditorConfig

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,17 +2,15 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({message: "Hello Bootcamp!!!"});
-});
+routes.get('/', (req, res) => res.json({ message: ' Hello Bootcamp!!! ' }));
 
 export default routes;
 
 // *********** Atalhos ******************************
-    // Ctrl + enter / adiciona comentarios
-    // Ctrl k Ctrl C comenta a linha atual
-    // Ctrl k Ctrl u descomenta a linha atual
-    // Ctrl k Ctrl s show keyboards shotcuts
-    // Ctrl Alt k copia a linha atual na de baixo
-    // Ctrl Shift k apaga linha atual
+// Ctrl + enter / adiciona comentarios
+// Ctrl k Ctrl C comenta a linha atual
+// Ctrl k Ctrl u descomenta a linha atual
+// Ctrl k Ctrl s show keyboards shotcuts
+// Ctrl Alt k copia a linha atual na de baixo
+// Ctrl Shift k apaga linha atual
 // **************************************************

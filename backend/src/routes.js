f5157a5d04e71107f6271bf6b36d9970cbd3a0a3
Danilo Pereira <danilopx1@gmail.com>
2020-02-03 11:46:46 -0300

    store appointments

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,12 +2,11 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
-// import User from './app/models/User';
-
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -23,18 +22,10 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
+routes.post('/appointments', AppointmentController.store);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
-/* routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Danilo Pereira',
-    email: 'danilopx@gmail.com',
-    password_hash: '1231423',
-  });
-
-  return res.json(user);
-});
-*/
 export default routes;
 
 // *********** Atalhos ******************************

ef954725ce4b7d211150b824965d77aa75214baf
Danilo Pereira <danilopx1@gmail.com>
2020-02-04 14:33:17 -0300

    Config templates e-mail(Nodemailer)

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  post: '2525',
+  secure: false,
+  auth: {
+    user: '06a1578745961e',
+    pass: 'b75d4e4abf458f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};

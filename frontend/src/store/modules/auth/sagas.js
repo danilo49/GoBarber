0ccd9f881934ce90f8cf3ca695f62a87d9470554
Danilo Pereira <danilopx1@gmail.com>
2020-02-27 21:31:19 -0300

    Configuring store

--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -1,3 +1,26 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import history from '../../../services/history';
 
-export default all([]);
+import api from '../../../services/api';
+
+import { signInSuccess } from './actions';
+
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+  console.tron.log(email, password);
+  const response = yield call(api.post, 'sessions', {
+    email,
+    password,
+  });
+
+  const { token, user } = response.data;
+
+  if (!user.provider) {
+    console.tron.error('Usuario nao é prestador');
+  }
+  yield put(signInSuccess(token, user));
+
+  history.push('/dashboard');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
